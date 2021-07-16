473eaebbb90c7db74a30345899b0e711db375ffb
Jesse Medrano <Jtamedrano@gmail.com>
2021-07-15 22:38:12 -0700

    Setup/init repo (#2)
    * created tsconfig, init package, and repo
    
    * created tsconfig, init package, and repo (#1)
    
    Looks good!
    
    * Init Apollo Server

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { createConnection } from "typeorm";
+import dotenv from "dotenv-safe";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import CustomerResolver from "./resolvers/customer_resolver";
+import { buildSchema } from "type-graphql";
+
+dotenv.config();
+
+const main = async () => {
+  await createConnection({
+    type: "mysql",
+    url: process.env.CLEARDB_DATABASE_URL,
+    entities: [],
+  });
+
+  const app = express();
+
+  const server = new ApolloServer({
+    schema: await buildSchema({ resolvers: [CustomerResolver] }),
+  });
+
+  server.applyMiddleware({
+    app,
+    cors: false,
+  });
+
+  const port = process.env.PORT as string;
+
+  app.listen(parseInt(port), () => {
+    console.log(`Server started on http://localhost:${port}`);
+    console.log(`GQL Playground on http://localhost:${port}/graphql`);
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
